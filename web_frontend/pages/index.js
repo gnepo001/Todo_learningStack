bd84e0c90bf98bdf13fc01c9a3c48340bf9d810e
Gilbert Nepomuceno <gilbertnepomuceno99@gmail.com>
2024-12-30 23:20:46 -0800

    fixed tailwind not working, and basic outline of landing page

--- web_frontend/pages/index.js
+++ web_frontend/pages/index.js
@@ -1,7 +1,15 @@
 import React from "react";
 
 const index = () => {
-  return <div>index</div>;
+  return (
+    <div>
+      <div className="bg-lime-600 p-6">
+        <h1>Todo List</h1>
+        <button>Add +</button>
+      </div>
+      {/* will be pulling info from django backend */}
+    </div>
+  );
 };
 
 export default index;
